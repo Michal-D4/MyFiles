5aab8dce53ac0a33879f94e28c7f5892951e9a02
mihal <mihal.d44@gmail.com>
2017-10-15 10:04:46 +0300

    Cont. TDD MyController 2

--- tests/controller/test_controler_view_dbu.py
+++ tests/controller/test_controler_view_dbu.py
@@ -23,80 +23,11 @@ class TestMyControllerViewDbu(unittest.TestCase):
         # self.connection.close()
         pass
 
-    def test_add_extension(self):
-        '''
-        check called_with for
-        1) dbu.select_other
-        2) dbu.insert_other
-        3) model.appendData
-        4) model.createIndex
-        5) extList.setCurrentIndex
-        '''
-        self.controller.dbu = Mock()
-
-        mock_select_other = Mock()
-        self.controller.dbu.select_other.return_value = mock_select_other
-        mock_select_other.fetchone.return_value = (0,)
-
-        self.controller.dbu.insert_other.return_value = 1
-
-        mock_model = Mock()
-        self.mock_view.extList.model.return_value = mock_model
-        mock_model.rowCount.return_value = 1
-        mock_model.createIndex.return_value = 'created index'
-
-        self.controller.add_extension('pdf')
-        self.controller.dbu.select_other.assert_called_with('HAS_EXT', ('pdf',))
-        self.controller.dbu.insert_other.assert_called_with('EXT', {'ext': 'pdf'})
-        mock_model.appendData.assert_called_with(('pdf', 1))
-        mock_model.createIndex.assert_called_with(0, 0, 0)
-        self.mock_view.extList.setCurrentIndex.assert_called_with('created index')
-
-    def test_allowed_removal(self):
-        mock_select_other = Mock()
-        self.mock_dbu.select_other.return_value = mock_select_other
-        mock_select_other.fetchone.return_value = ()    # allowed if not exist
-
-        res = self.controller.allowed_removal('allowed')
-        self.assertEqual(res, True)
-
-        mock_select_other.fetchone.return_value = (1,)  # not allowed == exist
-
-        res = self.controller.allowed_removal('not allowed')
-        self.assertEqual(res, False)
-
-    def test_remove_extension(self):
-        mock_ext_list = Mock()
-        self.controller.view.extList = mock_ext_list
-
-        mock_mod = Mock(spec_set=MyListModel)
-        mock_mod.data.return_value = 1
-        mock_ext_list.model.return_value = mock_mod
-
-        mock_index = Mock()
-        mock_index.row.return_value = 1
-        mock_ext_list.currentIndex.return_value = mock_index
-
-        self.controller.allowed_removal = Mock()
-        self.controller.allowed_removal.return_value = True
-
-        self.controller.remove_extension()
-        self.mock_dbu.delete_other.assert_called_with('EXT', (1,))
-        mock_mod.removeRows.assert_called_with(1)
-        mock_ext_list.setCurrentIndex.assert_called_with(mock_index)
-
     def test_populate_cb_places ( self ):
         self.mock_dbu.select_other.return_value = ((1, 2, 3), (4, 5, 6))
 
         self.controller.populate_cb_places()
         self.mock_view.cb_places.addItems.assert_called_once()
-
-    def test_on_change_data(self):
-        mock_add_place = Mock()
-        self.controller.add_place = mock_add_place
-
-        self.controller.on_change_data('cb_places', 'data')
-        mock_add_place.assert_called_once()
 
     def test_add_place(self):
         mock_ask_rename_or_new = Mock()
