b2cab8dd700bdf5c98a097776fb7bc2ad122a682
mihal <mihal.d44@gmail.com>
2017-10-29 07:47:15 +0300

    add several SQL statements related to changes in controller and tables

--- model/db_utils.py
+++ model/db_utils.py
@@ -1,26 +1,41 @@
-Selects = {'TREE': ('''WITH x(DirID, Path, ParentID, level) AS
- (SELECT DirID, Path, ParentID, 0 as level''',
-                    'FROM Dirs WHERE DirID = {}',
-                    'FROM Dirs WHERE ParentID = {}',
-                    '''UNION ALL
- SELECT t.DirID, t.Path, t.ParentID, x.level + 1 as lvl
- FROM x INNER JOIN Dirs AS t
- ON t.ParentID = x.DirID''',
-                    'and lvl <= {} ) SELECT * FROM x;',
-                    ') SELECT * FROM x order by ParentID desc, Path;'),
-           'PLACES': 'select * from myPlaces;',
-           'EXT': '''select ExtID+1000, Extension as title, GroupID as ID from Extensions 
-           UNION select GroupID, GroupName as title, 0 as ID from ExtGroups
-           order by ID desc, title;''',
+Selects = {'TREE':
+               ('WITH x(DirID, Path, ParentID, level) AS (SELECT DirID, Path, ParentID, 0 as level',
+                'FROM Dirs WHERE DirID = {} and PlaceId = {}',
+                'FROM Dirs WHERE ParentID = {} and PlaceId = {}',
+                ' '.join(('UNION ALL SELECT t.DirID, t.Path, t.ParentID,',
+                          'x.level + 1 as lvl FROM x INNER JOIN Dirs AS t',
+                          'ON t.ParentID = x.DirID')),
+                'and lvl <= {}) SELECT * FROM x order by ParentID desc, Path;',
+                ') SELECT * FROM x order by ParentID desc, Path;'),
+           'PLACES': 'select * from Places;',   # PlaceId starts with 0, where as other IDs with 1
+           'IS_EXIST': 'select * from Places where Place = ?;',
+           'EXT': ' '.join(('select ExtID+1000, Extension as title, GroupID',
+                            'as ID from Extensions UNION select GroupID,',
+                            'GroupName as title, 0 as ID from ExtGroups',
+                            'order by ID desc, title;')),
            'HAS_EXT': 'select count(*) from Extensions where Extension = ?;',
-           'EXT_IN_FILES': 'select FileID from Files where ExtID = ?;'}
+           'EXT_IN_FILES': 'select FileID from Files where ExtID = ?;',
+           'TAGS': 'select Tag, TagID from Tags order by Tag;',
+           'AUTHORS': 'select Author, AuthorID from Authors order by Author;',
+           'PLACE_IN_DIRS': 'select DirId from Dirs where PlaceId = ?;',
+           'FILE_TAGS': '',
+           'FILE_AUTHORS': ' '.join(('select Author from Authors where AuthorID in',
+                                     'select AuthorID from FileAuthor where FileID = ?;')),
+           'FILE_COMMENT': '',
+           'FILES_CUR_DIR': ' '.join(('select FileID, DirID, CommentID, FileName, Year,',
+                                      'Pages, Size from Files where DirId = ?;'))
+           }
 
-Insert = {'PLACES': '''insert into myPlaces (myPlaceId, myPlace, Title) values(?, ?, ?);''',
-          'EXT': '''insert into Extensions (Extension, GroupID) values (:ext, 0);''', }
+Insert = {'PLACES': 'insert into Places (Place, Title) values(?, ?);',
+          'EXT': 'insert into Extensions (Extension, GroupID) values (:ext, 0);',
+          'TAGS': 'insert into Tags (Tag) values (:tag);',
+          'AUTHORS': 'insert into Authors (Author) values (:author);'}
 
-Update = {'PLACES': 'update myPlaces set Title = ? where myPlaceId = ?;', }
+Update = {'PLACES': 'update Places set Title = ? where PlaceId = ?;',
+          'REMOVAL_DISK_INFO': 'update Places set Place = ? where PlaceId = ?;'}
 
-Delete = {'EXT': 'delete from Extensions where ExtID = ?;', }
+Delete = {'EXT': 'delete from Extensions where ExtID = ?;',
+          'PLACES': 'delete from Places where PlaceId = ?;'}
 
 class DBUtils:
     """Different methods for select, update and insert information into/from DB"""
@@ -29,49 +44,53 @@ class DBUtils:
         self.conn = connection
         self.curs = connection.cursor()
 
-    def dir_tree_select(self, dir_id, level):
+    def dir_tree_select(self, dir_id, level, place_id):
         """
         Select tree of directories starting from dir_id up to level
         :param dir_id: - starting directory, 0 - from root
         :param level: - max level of tree, 0 - all levels
         :return: cursor of directories
         """
-        sql = self.generate_sql(dir_id, level)
+        sql = self.generate_sql(dir_id, level, place_id)
 
         self.curs.execute(sql)
 
         return self.curs
 
-    def generate_sql(self, dir_id, level):
+    def generate_sql(self, dir_id, level, place_id):
         tree_sql = Selects['TREE']
-        tmp = (tree_sql[0], tree_sql[1].format(dir_id), tree_sql[2].format(dir_id),
-               tree_sql[3], tree_sql[4].format(level), tree_sql[5])
+        tmp = (tree_sql[0], tree_sql[1].format(dir_id, place_id),
+               tree_sql[2].format(dir_id, place_id), tree_sql[3],
+               tree_sql[4].format(level), tree_sql[5])
         cc = [(0, 2, 3, 5),
               (0, 1, 3, 5),
               (0, 2, 3, 4),
               (0, 1, 3, 4)]
-        i = (level > 0)*2 + (dir_id > 0)
+        i = (level > 0)*2 + (dir_id > 0)    # 00 = 0, 01 = 1, 10 = 2, 11 = 3
         sql = ' '.join([tmp[j] for j in cc[i]])
         return sql
 
     def select_other(self, sql, params=()):
+        # print('|---> select_other', sql, params)
+        # print(Selects[sql])
         self.curs.execute(Selects[sql], params)
         return self.curs
 
     def insert_other(self, sql, data):
+        # print(Insert[sql], data)
         self.curs.execute(Insert[sql], data)
         jj = self.curs.lastrowid
         self.conn.commit()
         return jj
 
     def update_other(self, sql, data):
-        print(Update[sql])
-        print('DBUtils.update_other data:', data)
+        # print('DBUtils.update_other data:', data)
+        # print(Update[sql])
         self.curs.execute(Update[sql], data)
         self.conn.commit()
 
     def delete_other(self, sql, data):
-        print(Delete[sql])
-        print('DBUtils.delete_other data:', data)
+        # print('DBUtils.delete_other data:', data)
+        # print(Delete[sql])
         self.curs.execute(Delete[sql], data)
         self.conn.commit()
--- tests/model/test_db_utils.py
+++ tests/model/test_db_utils.py
@@ -23,8 +23,7 @@ class TestDDBUtils(unittest.TestCase):
         mock_socket.gethostname.return_value = 'My place'
         create_db.create_all_objects(self.connection)
 
-        load_db = load_db_data.LoadDBData(self.connection, (0, 'place', 'title'))
-
+        load_db = load_db_data.LoadDBData(self.connection, ('place', 'title'))
         load_db.load_data(LOAD_DATA)
 
         self.dbu = DBUtils(self.connection)
@@ -33,40 +32,47 @@ class TestDDBUtils(unittest.TestCase):
         self.connection.close()
 
     def test_generate_sql_0_0(self):
-        sql = self.dbu.generate_sql(0, 0)
-        self.assertEqual(sql, '''WITH x(DirID, Path, ParentID, level) AS
- (SELECT DirID, Path, ParentID, 0 as level FROM Dirs WHERE ParentID = 0 UNION ALL
- SELECT t.DirID, t.Path, t.ParentID, x.level + 1 as lvl
- FROM x INNER JOIN Dirs AS t
- ON t.ParentID = x.DirID ) SELECT * FROM x order by ParentID desc, Path;''')
+        sql = self.dbu.generate_sql(0, 0, 0)
+        self.assertEqual(sql, ' '.join(('WITH x(DirID, Path, ParentID, level) AS',
+                                        '(SELECT DirID, Path, ParentID, 0 as level',
+                                        'FROM Dirs WHERE ParentID = 0 and PlaceId = 0',
+                                        'UNION ALL SELECT t.DirID, t.Path, t.ParentID,',
+                                        'x.level + 1 as lvl FROM x INNER JOIN Dirs AS t',
+                                        'ON t.ParentID = x.DirID ) SELECT * FROM x order',
+                                        'by ParentID desc, Path;')))
 
     def test_generate_sql_0_1(self):
-        sql = self.dbu.generate_sql(0, 1)
-        self.assertEqual(sql, '''WITH x(DirID, Path, ParentID, level) AS
- (SELECT DirID, Path, ParentID, 0 as level FROM Dirs WHERE ParentID = 0 UNION ALL
- SELECT t.DirID, t.Path, t.ParentID, x.level + 1 as lvl
- FROM x INNER JOIN Dirs AS t
- ON t.ParentID = x.DirID and lvl <= 1 ) SELECT * FROM x;''')
+        sql = self.dbu.generate_sql(0, 1, 0)
+        self.assertEqual(sql, ' '.join(('WITH x(DirID, Path, ParentID, level) AS',
+                                        '(SELECT DirID, Path, ParentID, 0 as level',
+                                        'FROM Dirs WHERE ParentID = 0 and PlaceId = 0',
+                                        'UNION ALL SELECT t.DirID, t.Path, t.ParentID,',
+                                        'x.level + 1 as lvl FROM x INNER JOIN Dirs AS t',
+                                        'ON t.ParentID = x.DirID and lvl <= 1)',
+                                        'SELECT * FROM x order by ParentID desc, Path;')))
 
     def test_generate_sql_1_0(self):
-        sql = self.dbu.generate_sql(1, 0)
-        self.assertEqual(sql, '''WITH x(DirID, Path, ParentID, level) AS
- (SELECT DirID, Path, ParentID, 0 as level FROM Dirs WHERE DirID = 1 UNION ALL
- SELECT t.DirID, t.Path, t.ParentID, x.level + 1 as lvl
- FROM x INNER JOIN Dirs AS t
- ON t.ParentID = x.DirID ) SELECT * FROM x order by ParentID desc, Path;''')
+        sql = self.dbu.generate_sql(1, 0, 0)
+        self.assertEqual(sql, ' '.join(('WITH x(DirID, Path, ParentID, level) AS',
+                                        '(SELECT DirID, Path, ParentID, 0 as level',
+                                        'FROM Dirs WHERE DirID = 1 and PlaceId = 0',
+                                        'UNION ALL SELECT t.DirID, t.Path, t.ParentID,',
+                                        'x.level + 1 as lvl FROM x INNER JOIN Dirs',
+                                        'AS t ON t.ParentID = x.DirID ) SELECT *',
+                                        'FROM x order by ParentID desc, Path;')))
 
     def test_generate_sql_1_1(self):
-        sql = self.dbu.generate_sql(1, 1)
-        self.assertEqual(sql, '''WITH x(DirID, Path, ParentID, level) AS
- (SELECT DirID, Path, ParentID, 0 as level FROM Dirs WHERE DirID = 1 UNION ALL
- SELECT t.DirID, t.Path, t.ParentID, x.level + 1 as lvl
- FROM x INNER JOIN Dirs AS t
- ON t.ParentID = x.DirID and lvl <= 1 ) SELECT * FROM x;''')
+        sql = self.dbu.generate_sql(1, 1, 0)
+        self.assertEqual(sql, ' '.join(('WITH x(DirID, Path, ParentID, level) AS',
+                                        '(SELECT DirID, Path, ParentID, 0 as level',
+                                        'FROM Dirs WHERE DirID = 1 and PlaceId = 0',
+                                        'UNION ALL SELECT t.DirID, t.Path, t.ParentID,',
+                                        'x.level + 1 as lvl FROM x INNER JOIN Dirs AS t',
+                                        'ON t.ParentID = x.DirID and lvl <= 1)',
+                                        'SELECT * FROM x order by ParentID desc, Path;')))
 
     def test_dir_tree_select_level_0_root_1(self):
-        '''level = 0 means all levels'''
-        cursor = self.dbu.dir_tree_select(2, 0)
+        cursor = self.dbu.dir_tree_select(2, 0, 1)
         self.assertIsNotNone(cursor)
 
         aa = tuple(cursor)
@@ -76,17 +82,16 @@ class TestDDBUtils(unittest.TestCase):
                               (2, 'f:\\Docs', 0, 0)))
 
     def test_dir_tree_select_level_1_root_1(self):
-        cursor = self.dbu.dir_tree_select(2, 1)
+        cursor = self.dbu.dir_tree_select(2, 1, 1)
         self.assertIsNotNone(cursor)
 
         aa = tuple(cursor)
-        self.assertEqual(aa, ((2, 'f:\\Docs', 0, 0),
-                              (1, 'f:\\Docs\\Box', 2, 1),
-                              (4, 'f:\\Docs\\Python', 2, 1)))
+        self.assertEqual(aa, ((1, 'f:\\Docs\\Box', 2, 1),
+                              (4, 'f:\\Docs\\Python', 2, 1),
+                              (2, 'f:\\Docs', 0, 0)))
 
     def test_dir_tree_select_level_0_root_0(self):
-        '''level = 0 means all levels'''
-        cursor = self.dbu.dir_tree_select(0, 0)
+        cursor = self.dbu.dir_tree_select(0, 0, 1)
         self.assertIsNotNone(cursor)
 
         aa = tuple(cursor)
@@ -97,19 +102,21 @@ class TestDDBUtils(unittest.TestCase):
                               (2, 'f:\\Docs', 0, 0)))
 
     def test_dir_tree_select_level_1_root_0(self):
-        cursor = self.dbu.dir_tree_select(0, 1)
+        cursor = self.dbu.dir_tree_select(0, 1, 1)
         self.assertIsNotNone(cursor)
 
         aa = tuple(cursor)
-        self.assertEqual(aa, ((2, 'f:\\Docs', 0, 0),
+        self.assertEqual(aa, ((1, 'f:\\Docs\\Box', 2, 1),
+                              (4, 'f:\\Docs\\Python', 2, 1),
                               (5, 'd:\\Doc2\\Java', 0, 0),
-                              (1, 'f:\\Docs\\Box', 2, 1),
-                              (4, 'f:\\Docs\\Python', 2, 1)))
+                              (2, 'f:\\Docs', 0, 0))
+)
 
     def test_select_other_PLACES(self):
         cursor = self.dbu.select_other('PLACES')
         aa = tuple(cursor)
-        self.assertEqual(aa, ((0, 'My place', 'My place'),))
+        self.assertEqual(aa, ((0, 'My place', 'My place'), (1, 'place', 'title')),
+                         msg='The 2 places inserted on SetUp')
 
     def test_select_other_EXT(self):
         cursor = self.dbu.select_other('EXT')
@@ -121,10 +128,66 @@ class TestDDBUtils(unittest.TestCase):
         aa = tuple(cursor)
         self.assertEqual(aa, ((1,),))
 
+        cursor = self.dbu.select_other('HAS_EXT', ('dummy', ))
+        aa = tuple(cursor)
+        self.assertEqual(aa, ((0,),))
+
     def test_select_other_EXT_IN_FILES(self):
         cursor = self.dbu.select_other('EXT_IN_FILES', (1, ))
         aa = tuple(cursor)
         self.assertEqual(aa, ((1,), (3,), (4,), (6,)))
+
+        cursor = self.dbu.select_other('EXT_IN_FILES', (0, ))
+        aa = tuple(cursor)
+        self.assertEqual(aa, ())
+
+    def test_select_other_IS_EXIST(self):
+        cursor = self.dbu.select_other('IS_EXIST', ('My place', ))
+        aa = tuple(cursor)
+        self.assertEqual(aa, ((0, 'My place', 'My place'),),
+                         msg="'My place' is s first place inserted on SetUp")
+
+        cursor = self.dbu.select_other('IS_EXIST', ('Your place', ))
+        aa = tuple(cursor)
+        self.assertEqual(aa, ())
+
+    def test_insert_other_PLACES(self):
+        res = self.dbu.insert_other('PLACES', ('new disk', 'new place'))
+        self.assertEqual(res, 2, msg='2 places inserted on SetUp, must be 2, but not!!!')
+
+    def test_insert_select_other_TAGS(self):
+        cursor = self.dbu.select_other('TAGS')
+        aa = tuple(cursor)
+        self.assertEqual(aa, ())
+
+        res = self.dbu.insert_other('TAGS', ('tag1',))
+        self.assertEqual(res, 1, msg='First record, but returned {}'.format(res))
+        cursor = self.dbu.select_other('TAGS')
+        aa = tuple(cursor)
+        self.assertEqual(aa, (('tag1', 1),))
+
+        res = self.dbu.insert_other('TAGS', ('tag2',))
+        self.assertEqual(res, 2, msg='Second record, but returned {}'.format(res))
+        cursor = self.dbu.select_other('TAGS')
+        aa = tuple(cursor)
+        self.assertEqual(aa, (('tag1', 1), ('tag2', 2)))
+
+    def test_insert_select_other_AUTHORS(self):
+        cursor = self.dbu.select_other('AUTHORS')
+        aa = tuple(cursor)
+        self.assertEqual(aa, ())
+
+        res = self.dbu.insert_other('AUTHORS', ('Author 1',))
+        self.assertEqual(res, 1, msg='First record, but returned {}'.format(res))
+        cursor = self.dbu.select_other('AUTHORS')
+        aa = tuple(cursor)
+        self.assertEqual(aa, (('Author 1', 1),))
+
+        res = self.dbu.insert_other('AUTHORS', ('Author 2',))
+        self.assertEqual(res, 2, msg='Second record, but returned {}'.format(res))
+        cursor = self.dbu.select_other('AUTHORS')
+        aa = tuple(cursor)
+        self.assertEqual(aa, (('Author 1', 1), ('Author 2', 2)))
 
 
 if __name__ == '__main__':
