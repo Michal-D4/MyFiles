467a496c7dc22bff5b9fdce3ece52b2480e17596
mihal <mihal.d44@gmail.com>
2017-11-28 21:54:58 +0300

    change widget for editing tags and authors

--- view/item_edit.py
+++ view/item_edit.py
@@ -1,52 +1,95 @@
 # view/item_edit.py
 
+import re
 from PyQt5.QtWidgets import QDialog
+from PyQt5.QtCore import Qt
 from view.ui_items_edit import Ui_ItemChoice
 from controller.my_qt_model import TableModel
 
 
 class ItemEdit(QDialog):
-    def __init__(self, labels, items, parent=None):
+    def __init__(self, titles, nodes, parent=None):
         super(ItemEdit, self).__init__(parent)
         self.view = Ui_ItemChoice()
         self.view.setupUi(self)
 
-        self.view.label_1.setText(labels[0])
-        self.view.label_2.setText(labels[1])
-        self.setWindowTitle(labels[2])
+        self.view.label_1.setText(titles[0])
+        self.view.label_2.setText(titles[1])
+        self.setWindowTitle(titles[2])
 
-        self.list_items = items
+        self.list_items = nodes
 
-        aa = max(items, key=lambda t: len(t))
-        self.max_width = self.view.items.fontMetrics().boundingRect(aa).width() + 10
-        w = self.view.items.size().width()
-        col_no = w // self.max_width
-        print('|---> __init__:', col_no, w, self.max_width)
-        self.setup_model(col_no)
+        aa = max(nodes, key=lambda t: len(t))
+        self.max_width = self.view.items.fontMetrics().boundingRect(aa).width() + 20
+        self.setup_view()
 
         self.view.items.resizeEvent = self.resize_event
 
+    def setup_view(self):
+        w = self.view.items.size().width()
+        col_no = w // self.max_width
+        print('|---> setup_view:', col_no, w, self.max_width)
+        self._setup_model(col_no)
+
     def resize_event(self, event):
         w = event.size().width()
-        # w_old = event.
         col_no = w // self.max_width
         print('|---> resize_event', w, col_no, self.view.items.model().columnCount())
         if col_no != self.view.items.model().columnCount():
-            self.setup_model(col_no)
+            self._setup_model(col_no)
 
-    def setup_model(self, col_no):
+    def selection_changed(self, selected, deselected):
+        """
+        Changed selection in self.view.items
+        :param selected:   QList<QModelIndex>
+        :param deselected: QList<QModelIndex>
+        :return: None
+        """
+        print('|---> selection_changed')
+        txt = self.view.in_field.toPlainText()
+        idx = selected.indexes()
+        if idx:
+            for jj in idx:
+                tt = self.view.items.model().data(jj)
+                txt = ', '.join((txt, tt)) if txt else tt
+
+        id2 = deselected.indexes()
+        if id2:
+            for jj in id2:
+                txt = re.sub(' '.join((',', self.view.items.model().data(jj))), '', txt)
+
+        self.view.in_field.setText(txt)
+
+
+    def _setup_model(self, col_no):
         row_no = len(self.list_items) // col_no + 1
         aa = []
         for i in range(row_no):
-            aa.append(tuple(self.list_items[i * col_no:(i + 1) * col_no]))
-        model = TableModel(parent=self.view.items)
+            aa.append(tuple(self.list_items[i*col_no: (i + 1)*col_no]))
+
+        model = TableModel2(parent=self.view.items)
         model.setColumnCount(col_no)
+
         for a in aa:
             model.append_row(a)
+
         self.view.items.setModel(model)
-        for i in range(1, col_no):
-            self.view.items.header().resizeSection(i, self.max_width)
-            print('width of col {}'.format(i), self.view.items.columnWidth(i), self.max_width)
+
+        for i in range(0, col_no):
+            self.view.items.horizontalHeader().resizeSection(i, self.max_width)
+
+        self.view.items.selectionModel().selectionChanged.connect(self.selection_changed)
+
+
+class TableModel2(TableModel):
+    def __init__(self, parent=None, *args):
+        super().__init__(parent, *args)
+
+    def data(self, index, role=Qt.DisplayRole):
+        if role == Qt.TextAlignmentRole:
+            return Qt.AlignRight
+        return super().data(index, role)
+
 
 if __name__ == "__main__":
     import sys
@@ -70,7 +113,8 @@ if __name__ == "__main__":
     items = ['item {}'.format(i+1) for i in range(25)]
 
     ItemChoice = ItemEdit(labels, items)
-    ItemChoice.resize(100, 300)
+    ItemChoice.resize(500, 300)
+
     ItemChoice.show()
     sys.exit(app.exec_())
 
