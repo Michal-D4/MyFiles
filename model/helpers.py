eba4bda07f7540755f19bccb40fe309c9ffe1c49
mihal <mihal.d44@gmail.com>
2018-01-03 07:53:09 +0300

    add method get_parent_dir

--- model/helpers.py
+++ model/helpers.py
@@ -1,8 +1,12 @@
 # model/helpers.py
+import os
 
 
 def get_file_extension(file_name):
     if file_name.rfind('.') > 0:
         return str.lower(file_name.rpartition('.')[2])
     return ''
+
+def get_parent_dir(path):
+    return path.rpartition(os.altsep)[0]
 
