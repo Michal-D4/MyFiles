a8d51e4f6b579505ebfb6ace5a172d4c2df1c6b2
mihal <mihal.d44@gmail.com>
2018-01-07 09:47:17 +0300

    UPDATE_TAG SQL to rename tag

--- model/utilities.py
+++ model/utilities.py
@@ -94,7 +94,8 @@ Update = {'PLACE_TITLE': 'update Places set Title = :title where PlaceId = :plac
           'FILE_COMMENT': 'update Files set CommentID = ? where FileID = ?;',
           'PAGES': 'update Files set Pages = ? where FileID = ?;',
           'OPEN_DATE': "update Files set Opened = date('now') where FileID = ?;",
-          'COMMENT_DATE': "update Files set Commented = date('now') where FileID = ?;"
+          'COMMENT_DATE': "update Files set Commented = date('now') where FileID = ?;",
+          'UPDATE_TAG': 'update Tags set Tag = ? where TagID = ?;'
           }
 
 Delete = {'EXT': 'delete from Extensions where ExtID = ?;',
