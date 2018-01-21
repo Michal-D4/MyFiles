17c58e08316c5e95869d0f09166f91e091f38681
mihal <mihal.d44@gmail.com>
2018-01-21 18:28:13 +0300

    setup ??

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='myFiles',
+    version='0.1',
+    packages=['controller', 'model', 'model.utils', 'view'],
+    url='https://github.com/Michal-D4/MyFiles',
+    license='',
+    author='michal',
+    author_email='mihal.d44@gmail.com',
+    description='manage my files'
+)
